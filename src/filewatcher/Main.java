aab0990bf548779e20095b6db79b0de998d03be9
Mariam <mah01@uw.edu>
2026-05-24 18:48:38 -0700

    update Main.java

--- src/filewatcher/Main.java
+++ src/filewatcher/Main.java
@@ -2,6 +2,12 @@ package filewatcher;
 
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            // load the SQLite driver before the app starts
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            System.out.println("SQLite driver not found: " + e.getMessage());
+        }
+        new MainForm();
     }
 }
